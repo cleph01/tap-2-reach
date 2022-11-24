40f7e922e0559cb182addc99e660adee24da8538
Charlie <charles.j.montoya@gmail.com>
2022-11-23 20:34:39 -0500

    dashboard layout set with option boxes

--- components/business/dash/OptionBox.js
+++ components/business/dash/OptionBox.js
@@ -4,7 +4,7 @@ import styles from "../../../styles/components/business/dash/OptionBox.module.sc
 function OptionBox({ optionName, emoji }) {
     return (
         <div className={styles.container}>
-            <div className={style.wrapper}>
+            <div className={styles.wrapper}>
                 <div>{optionName}</div>
                 <div>{emoji}</div>
             </div>
--- pages/business/dash.js
+++ pages/business/dash.js
@@ -10,11 +10,19 @@ function dash() {
             <div className={styles.wrapper}>
                 <div className={styles.header}>
                     <OptionBox optionName="Earned Reviews" emoji="star" />
+
+                    <OptionBox optionName="Opt Ins" emoji="Check" />
+
+                    <OptionBox optionName="Opt Outs" emoji="X" />
                 </div>
                 <div className={styles.body}>
                     <OptionBox optionName="Opt Ins" emoji="Check" />
                 </div>
                 <div className={styles.footer}>
+                    <OptionBox optionName="Earned Reviews" emoji="star" />
+
+                    <OptionBox optionName="Opt Ins" emoji="Check" />
+
                     <OptionBox optionName="Opt Outs" emoji="X" />
                 </div>
             </div>
